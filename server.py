6b6e31fe1558867f9fa64143d8e3495dec75dec0
Jack Bridger <jacksbridger@gmail.com>
2020-04-19 12:51:38 +0100

    add gitignore

--- server.py
+++ server.py
@@ -7,8 +7,7 @@ loaded_model = pickle.load(open(filename, 'rb'))
 
 @app.route('/good')
 def good_example_func():
-    good_example = [[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,50,4990,0]]
-    bad_example = [[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,50,499,0]]
+    good_example = [[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,50,499,0]]
     prediction = loaded_model.predict(good_example)[0]
     if prediction == 0:
         return "I predict a failure"
@@ -16,7 +15,6 @@ def good_example_func():
         return "I predict success"
 @app.route('/bad')
 def bad_example_func():
-    good_example = [[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,50,4990,0]]
     bad_example = [[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,50,499,0]]
     prediction = loaded_model.predict(bad_example)[0]
     if prediction == 0:
